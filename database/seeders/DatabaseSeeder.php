96d2fc64dd7b6c42057e5dd071a2e2d6f524b4c7
Ashbee Morgado <ashbee.morgado@icloud.com>
2020-10-27 22:36:18 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Person;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        Person::factory(5000)->create();
+    }
+}
---
+++ resources/js/components/RandomPickerComponent.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="site-wrapper video-background">
+    
+
+    
+
+    <div class="site-wrapper-inner" >
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">LIGHT MICROFINANCE INC</h3>
+                    <nav>
+                        <ul class="nav masthead-nav">
+                            <li><a href="https://twitter.com/jenyelkind"><i class="fa fa-twitter fa-fw"></i></a></li>
+                            <li><a href="https://www.facebook.com/jenyelkind"><i class="fa fa-facebook fa-fw"></i></a></li>
+                            <li><a href="https://plus.google.com/+JenyElkind/"><i class="fa fa-google-plus fa-fw"></i></a></li>
+                            <li><a href="mailto:elkind@elkind.net"><i class="fa fa-envelope fa-fw"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="cover-container" style="margin-top:250px">
+            <div class="inner cover">
+                <h1 class="cover-heading">Random Picker</h1>
+                <p class="lead">Random picker for sir enmar</p>
+                <p class="lead"><a href="http://forbetterweb.com/htmlandbootstrap/classic-cover-video-background-one-page-websites-html-bootstrap/" class="btn btn-lg btn-default">Go</a></p>
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>Copyright &copy; <a href="/">primoashbee</a></p>
+                    
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        
+    }
+}
+</script>
